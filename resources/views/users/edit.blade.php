09af4dff85411b531dc4adc79ace87eba7429174
AngelP11 <angelgabrielponce@gmail.com>
2018-07-04 11:16:45 -0400

    Modificacion del formulario para editar users

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -6,27 +6,13 @@
 
     {!! Form::model($user, ['route' => ['users.update', $user->id], 'method' => 'PUT']) !!}
 
-        <div class="row">
-            <div class="col-6">
-                <div class="form-group">
-                    <label for="username">Nombre de Usuario</label>
-                    <input class="form-control" type="text" name="username" id="username" value="{{ $user->username }}">
-                </div>
-            </div>
-            <div class="col-6">
-                <div class="form-group">
-                    <label for="email">Correo Electronico</label>
-                    <input class="form-control" type="email" name="email" id="email" value="{{ $user->email }}">
-                </div>
-            </div>
-        </div>
-
         <div class="form-group">
-            <label for="role">Rol</label>
-            <select class="form-control" name="role" id="role">
-                <option value="1">Administrador</option>
-                <option value="2">Estudiante</option>
-            </select>
+            <label for="username">Nombre de Usuario</label>
+            <input class="form-control" type="text" name="username" id="username" value="{{ $user->username }}">
+        </div>
+        <div class="form-group">
+            <label for="email">Correo Electronico</label>
+            <input class="form-control" type="email" name="email" id="email" value="{{ $user->email }}">
         </div>
 
         <button class="btn btn-success" type="submit">Guardar cambios</button>
